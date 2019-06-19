34ee0c131d4c58f601c84b18e672f9770181fa2c
Tecco2233 <biel.tecco@gmail.com>
2019-06-19 12:05:06 -0300

    seusudia

---
+++ src/pacaje/qqdsadsa.java
@@ -0,0 +1,15 @@
+package pacaje;
+
+import java.util.*;
+
+public class qqdsadsa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner qw = new Scanner(System.in);
+		System.out.println("saskdsajhiudsahodmnsai onoa");
+		int a = qw.nextInt();
+		System.out.println(a);
+	}
+
+}
