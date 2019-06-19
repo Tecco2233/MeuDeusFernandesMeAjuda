995dbc628d14e0308329b667e23741a2df451feb
Tecco2233 <biel.tecco@gmail.com>
2019-06-19 12:13:42 -0300

    eunemsi

---
+++ src/pacaje/ehodoismeuquerido.java
@@ -0,0 +1,16 @@
+package pacaje;
+
+import java.util.Scanner;
+
+public class ehodoismeuquerido {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner qw = new Scanner(System.in);
+		System.out.println("saskdsajhiudsahodmnsai onoa");
+		int a = qw.nextInt(); //Isso n serve de nada na vdd mas tá ai meu querido <3
+		System.out.println(a);
+	
+	}
+
+}
